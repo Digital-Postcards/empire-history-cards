01ef512e380ebc12aa0987e04b4ad791b899adba
parthiv-m <parthivmenon.dev@gmail.com>
2024-10-23 00:59:18 -0400

    Modify choice card component to be text heavy

---
+++ frontend/src/components/dashboard/wiki/choicecard.tsx
@@ -0,0 +1,24 @@
+import { ArrowRight } from "lucide-react";
+import { Button } from "shadcn/components/ui/button";
+import { ChoiceCardProps } from "types";
+
+const ChoiceCard = (props: ChoiceCardProps) => {
+    return (
+        <div className="border-none shadow-none p-24 w-full hover:bg-neutral-100/25">
+            <div className="px-0">
+                <h2 className="text-2xl font-bold">{props?.title}</h2>
+            </div>
+            <div className="px-0 mt-3">
+                {props?.children}
+            </div>
+            <div className="flex gap-2 px-0 mt-4">
+                <Button size={"lg"} variant={"outline"} onClick={() => window.location.href = "/exhibit/map?type=" + props?.type}>View on map</Button>
+                <Button className="grow px-3" size={"lg"} onClick={() => window.location.href = "/exhibit/wiki/list?type=" + props?.type}>
+                    View all<span className="flex-grow"></span><ArrowRight />
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default ChoiceCard
--- frontend/src/pages/dashboard/wiki/index.tsx
+++ frontend/src/pages/dashboard/wiki/index.tsx
@@ -3,19 +3,25 @@ import { ChoiceCard } from "components/dashboard/wiki";
 const Wiki = () => {
     return (
         <>
-            <div className="col-span-5 flex justify-center items-center gap-16">
+            <div className="col-span-8 flex justify-center">
                 <ChoiceCard
                     type="postcard"
                     title="Postcards"
-                    description="Lorem ipsum odor amet, consectetuer adipiscing elit. Tempus sociosqu phasellus vestibulum scelerisque dignissim? Hac penatibus non tellus eget mus laoreet. Ipsum aenean magnis massa enim enim."
                     image="/images/wiki/three.jpg"
-                />
+                >
+                    <p>Postcards, as we know them today, are small, rectangular cards with an image on one side and space for a message and address on the verso, that serve as souvenirs from an event or location.  </p>
+                    <p className="mt-2">Although they occupy a more passive role in today's popular culture, picture postcards historically served an imperial agenda, and were widely collected in the age of New Imperialism and Jim Crow. In the early 1900s, known as the “golden age of postcards”, these images functioned as tools of empire, constructing European and American understandings of non-white “others”. Prior to the integration of postcards into popular media, wealthy patrons had been the sole commissioners and owners of detailed paintings representing colonized or enslaved people, which they displayed in private households. </p>
+                    <p className="mt-2">The low cost, reproducibility, and global circulation of postcards made them an accessible and influential tool in constructing hegemonic whiteness and its understanding of racialized “others”. Portable visual media was able to democratize the colonial gaze and encourage participation in empire building, as the postcard tangibly represented the spoils of imperial expansion and perpetuated stereotypes about marginalized people.</p>
+                </ChoiceCard>
                 <ChoiceCard
                     type="tradecard"
                     title="Tradecards"
-                    description="Lorem ipsum odor amet, consectetuer adipiscing elit. Tempus sociosqu phasellus vestibulum scelerisque dignissim? Hac penatibus non tellus eget mus laoreet. Ipsum aenean magnis massa enim enim."
                     image="/images/wiki/seven.jpg"
-                />
+                >
+                 <p>Trade cards played a critical role in advertisements in the late 1800s, which was also the period of rapid industrial expansion in North America and Europe. These small, portable, and collectible cards displayed information about companies and their products in the context of booming industrial production, while also circulating illustrations which contributed to notions of hegemonic whiteness and the superiority of modern empires based on industrial capitalism.</p>
+                    <p className="mt-2">The passionate collection of trade cards in the late 1800s also meant, indirectly, the passionate consumption of racialized and gendered media. The roots of many modern-day racist, sexist, and classist stereotypes can be traced through the proliferation of these cards. Although trade cards did not invent these racialized and gendered figures, they broadened their reach to common people.</p>
+                    <p className="mt-2">Scientific racism was limited to academic circles, but trade cards and later postcards, democratized race science, making it accessible to working-class white people, through everyday commodities, a phenomenon historians have called “commodity racism”.</p>   
+                </ChoiceCard>
             </div>
         </>
     )
