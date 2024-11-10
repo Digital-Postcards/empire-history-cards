b520565f26068e6ac1f456571f5c3e83de84bfa3
parthiv-m <parthivmenon.dev@gmail.com>
2024-11-10 13:52:37 -0500

    Minor change to loader props

--- frontend/src/components/common/loader.tsx
+++ frontend/src/components/common/loader.tsx
@@ -1,7 +1,7 @@
 const LoaderDiv = () => {
     return <div className="loader"></div>;
 }
-const Loader = (props: { isFullSize: boolean, classes: string }) => {
+const Loader = (props: { isFullSize?: boolean, classes?: string }) => {
     if (props?.isFullSize)
         return (
             <div className={`min-h-screen w-screen flex justify-center items-center ${props?.classes}`}>
