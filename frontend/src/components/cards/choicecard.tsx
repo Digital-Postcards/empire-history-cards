0ca40ac47451b5441f9cb8e4b87a241244173bed
parthiv-m <parthivmenon.dev@gmail.com>
2024-11-14 21:36:21 -0500

    Add image background for card choice component

--- frontend/src/components/cards/choicecard.tsx
+++ frontend/src/components/cards/choicecard.tsx
@@ -4,19 +4,39 @@ import { ChoiceCardProps } from "types";
 
 const ChoiceCard = (props: ChoiceCardProps) => {
     return (
-        <div className={`shadow-none lg:p-24 md:p-16 p-8 w-full hover:bg-neutral-100/45 ` + props?.classes}>
-            <div className="px-0">
-                <h2 className="text-2xl font-bold">{props?.title}</h2>
+        <div
+            style={{
+                background: "url(" + props?.image + ")",
+                backgroundRepeat: "none",
+                backgroundSize: "cover",
+                backgroundAttachment: "fixed",
+                backgroundPosition: "center",
+            }}
+            className={`relative lg:p-24 md:p-16 p-8 w-full ` + props?.classes}
+        >
+            <div className="z-10 relative peer">
+                <div className="px-0 text-white">
+                    <h2 className="text-2xl font-bold">{props?.title}</h2>
+                </div>
+                <div className="px-0 mt-3 text-white">
+                    {props?.children}
+                </div>
+                <div className="flex lg:flex-row flex-col gap-2 px-0 mt-4">
+                    <Button size={"lg"} variant={"outline"} onClick={() => window.location.href = "/map?type=" + props?.type}>View on map</Button>
+                    <Button className="grow px-3" size={"lg"} onClick={() => window.location.href = "/cards?type=" + props?.type}>
+                        View all<span className="flex-grow"></span><ArrowRight />
+                    </Button>
+                </div>
             </div>
-            <div className="px-0 mt-3">
-                {props?.children}
-            </div>
-            <div className="flex lg:flex-row flex-col gap-2 px-0 mt-4">
-                <Button size={"lg"} variant={"outline"} onClick={() => window.location.href = "/map?type=" + props?.type}>View on map</Button>
-                <Button className="grow px-3" size={"lg"} onClick={() => window.location.href = "/cards?type=" + props?.type}>
-                    View all<span className="flex-grow"></span><ArrowRight />
-                </Button>
-            </div>
+            <div
+                className="
+                            absolute h-full w-full left-0 top-0 
+                            bg-black/40 hover:bg-neutral-900/65 peer-hover:bg-neutral-900/65
+                            backdrop-blur-sm peer-hover:backdrop-blur-md hover:backdrop-blur-md 
+                            transition-all duration-200
+                            z-0
+                        "
+            ></div>
         </div>
     )
 }
--- frontend/src/components/common/choiceofcards.tsx
+++ frontend/src/components/common/choiceofcards.tsx
@@ -2,19 +2,19 @@ import { ChoiceCard } from "components/cards";
 
 const ChoiceOfCards = (props: { classes?: string }) => {
     return (
-        <div className={`w-full grid lg:grid-cols-2 grid-cols-1 border border-neutral-400 ` + props?.classes}>
+        <div className={`w-full grid lg:grid-cols-2 grid-cols-1 ` + props?.classes}>
             <ChoiceCard
                 type="postcard"
                 title="Postcards"
                 image="/images/carousel/one.jpg"
-                classes="border border-black border-0 border-b lg:border-r lg:border-b-0"
+                classes="border border-white border-0 border-b lg:border-r lg:border-b-0"
             >
                 <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Blandit penatibus neque maximus cursus conubia. Facilisi bibendum est magna egestas, massa sollicitudin. Pellentesque tristique nascetur netus lobortis porttitor aenean. Tempus sit congue ligula fusce nulla molestie mi ex varius. Tortor ridiculus efficitur ad inceptos mus lectus diam. Luctus platea mauris scelerisque condimentum finibus sapien primis platea. Varius taciti fusce venenatis molestie eget erat nisi. Ornare nostra lobortis sem vestibulum purus phasellus faucibus cubilia. Sapien justo libero auctor aliquam orci velit fermentum.</p>
             </ChoiceCard>
             <ChoiceCard
                 type="tradecard"
                 title="Tradecards"
-                image="/images/carousel/one.jpg"
+                image="/images/wiki/one.jpg"
             >
                 <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Blandit penatibus neque maximus cursus conubia. Facilisi bibendum est magna egestas, massa sollicitudin. Pellentesque tristique nascetur netus lobortis porttitor aenean. Tempus sit congue ligula fusce nulla molestie mi ex varius. Tortor ridiculus efficitur ad inceptos mus lectus diam. Luctus platea mauris scelerisque condimentum finibus sapien primis platea. Varius taciti fusce venenatis molestie eget erat nisi. Ornare nostra lobortis sem vestibulum purus phasellus faucibus cubilia. Sapien justo libero auctor aliquam orci velit fermentum.</p>
             </ChoiceCard>
