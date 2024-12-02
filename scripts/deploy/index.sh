cbd34cc4410779164e72b25e8c0d62ac8d22d69e
parthiv-m <parthivmenon.dev@gmail.com>
2024-12-01 22:42:07 -0500

    Add deploy script for server use

--- scripts/deploy/index.sh
+++ scripts/deploy/index.sh
@@ -2,10 +2,26 @@
 
 # pull latest changes
 git pull
+
+# update the frontend
+
 cd "$(dirname "${BASH_SOURCE[0]}")"/
 # install dependencies in case any have changed
 cd ../../frontend && npm ci
+# build the frontend
+npm run build
+# copy build files to the apache directory
+sudo cp -r build/* /var/www/digitalhum
+
+# update the backend
+
 cd "$(dirname "${BASH_SOURCE[0]}")"/
 # install dependencies in case any have changed
 cd ../../server && npm ci
+# build the backend
+npm run build
+# restart the backend service using pm2
+pm2 restart dh-api
 
+# save pm2 state
+pm2 save
