cf6742461f11e9a803c41fa4bf5978a3fe6c2413
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2025-04-29 17:57:12 -0400

    fix carousel images styling

--- frontend/src/components/single/carousel.tsx
+++ frontend/src/components/single/carousel.tsx
@@ -3,35 +3,34 @@ import { useState, useEffect } from "react";
 // Dynamically import images using Webpack's `require.context` method
 const images = require.context("/public/images/carousel", false, /\.(jpg|jpeg|png|gif)$/);
 
-const imagePaths = images.keys(); // Get all the file paths of the images
+const imagePaths = images.keys();
 
 const HeaderCollage = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    // Handle the transition to the next slide
     const nextSlide = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % imagePaths.length);
     };
 
-    // Set an interval to change the slide every 5 seconds
     useEffect(() => {
         const interval = setInterval(nextSlide, 5000);
-        return () => clearInterval(interval); // Clean up interval on component unmount
+        return () => clearInterval(interval);
     }, []);
 
     return (
-        <div className="w-screen fixed top-0 h-screen overflow-hidden">
+        <div className="w-screen fixed top-0 h-screen overflow-hidden bg-light-gray">
             <div
                 className="flex transition-transform duration-700 ease-in-out"
                 style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
                 {imagePaths.map((image, index) => (
-                    <div
-                        key={index}
-                        style={{
-                            background: `url(${images(image)}) center/cover no-repeat`,
-                        }}
-                        className="w-screen h-[45vh] flex-shrink-0"
-                    />
+                    <div key={index} className="w-screen h-[45vh] flex-shrink-0 bg-white flex justify-center">
+                        <img
+                            src={images(image)}
+                            alt={`Slide ${index}`}
+                            className="object-contain max-h-full"
+                            style={{ alignSelf: "flex-start" }}
+                        />
+                    </div>
                 ))}
             </div>
         </div>
