8937a50e220d07b1dcb48aad30eaecaf51e8e3bc
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2025-05-06 00:29:34 -0400

    update about page

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -10,19 +10,19 @@ const About = () => {
             <CardLookalikeWithImage
                 centered
                 rotate="-rotate-3"
-                imageURL="/images/about/history-team.jpeg"
-                caption="The History Team from The College of New Jersey: (from left) Sarah Adamo, Sapphire Srigley, Abhishta Tantry, George Kapetanakis, Satya Shikha Chakraborty, Carolyn Carmody, Phoebe Abeles, and Lucy Fleischmann."
+                imageURL="/images/about/history_team_2022.jpeg"
+                caption="Exhibition curated by Prof. Chakraborty and her history students at the TCNJ Art Gallery in 2022. (from left) Sarah Adamo, Sapphire Srigley, Abhishta Tantry, George Kapetanakis, Satya Shikha Chakraborty, Carolyn Carmody, Phoebe Abeles, and Lucy Fleischmann."
             />
             <p className="mt-12 text-justify" dangerouslySetInnerHTML={{ __html: ABOUT_PAGE_CONTENT.paragraph3 }} />
             <div className="flex md:flex-row flex-col">
                 <CardLookalikeWithImage
-                    imageURL="/images/about/seminar.jpg"
-                    caption="The Seminar culminated in a History Exhibition curated by Prof. Chakraborty and the History/ Art history students at the TCNJ Art Gallery in May 2022."
-                />
-                <CardLookalikeWithImage
                     rotate="rotate-3"
                     imageURL="/images/about/2024.jpg"
                     caption="In 2024, TCNJ History students Marisa Valdes and Benjamin Lieberman worked on the project."
+                />
+                <CardLookalikeWithImage
+                    imageURL="/images/about/history_team_2025.jpeg"
+                    caption="In 2025, Arefa Khajanchi, Kay Reed, and Madison Moran worked on the project, analyzing postcards from the French, Dutch, and British empires."
                 />
             </div>
             <p className="mt-12 text-justify" dangerouslySetInnerHTML={{ __html: ABOUT_PAGE_CONTENT.paragraph4 }} />
